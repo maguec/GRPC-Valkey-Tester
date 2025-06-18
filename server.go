610e54375cbb7a69575fb85398521491deacadb2
Chris Mague <github@mague.com>
2025-06-18 15:05:30 -0700

    utilize several client connections

--- server.go
+++ server.go
@@ -23,7 +23,7 @@ const (
 // Define a struct that implements the generated gRPC service interface
 type server struct {
 	pb.UnimplementedGreeterServer
-	valkeyClient valkey.Client
+	valkeyClients []valkey.Client
 }
 
 func dialLogger(s1 string, dialer *net.Dialer, tlsconfig *tls.Config) (net.Conn, error) {
@@ -35,37 +35,43 @@ func dialLogger(s1 string, dialer *net.Dialer, tlsconfig *tls.Config) (net.Conn,
 
 // Implement the SayHello method
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+  c := rand.Intn(10)
+  fmt.Println("Using client", c)
+  client := s.valkeyClients[c]
 	cmds := make(valkey.Commands, 0, 100)
 	for i := 0; i < 10; i++ {
 		mykey := fmt.Sprintf("key-%d", rand.Intn(100000))
-		cmds = append(cmds, s.valkeyClient.B().Set().Key(mykey).Value("val").Build())
+		cmds = append(cmds, client.B().Set().Key(mykey).Value("val").Build())
 	}
-	for _, resp := range s.valkeyClient.DoMulti(ctx, cmds...) {
+	for _, resp := range client.DoMulti(ctx, cmds...) {
 		if err := resp.Error(); err != nil {
 			panic(err)
 		}
 	}
-	time.Sleep(300 * time.Millisecond)
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
-	client, err := valkey.NewClient(
-		valkey.ClientOption{
-			InitAddress: []string{"127.0.0.1:30001"},
-			DialFn:      dialLogger,
-		},
-	)
-	if err != nil {
-		panic(err)
+  clients := make([]valkey.Client, 10)
+	for i := 0; i < 10; i++ {
+		client, err := valkey.NewClient(
+			valkey.ClientOption{
+				InitAddress: []string{"127.0.0.1:30001"},
+				DialFn:      dialLogger,
+			},
+		)
+		if err != nil {
+			panic(err)
+		}
+		defer client.Close()
+    clients[i] = client
 	}
-	defer client.Close()
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{valkeyClient: client})
+	pb.RegisterGreeterServer(s, &server{valkeyClients: clients})
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
