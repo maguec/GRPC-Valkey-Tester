f7787b71151c12a7f8849bc152414c54013e9a76
Chris Mague <github@mague.com>
2025-06-25 08:00:24 -0700

    print stats and make the runs and workers configurable

--- client.go
+++ client.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/jamiealquiza/tachymeter"
+	"github.com/maguec/metermaid"
 	"golang.org/x/exp/rand"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	"github.com/alexflint/go-arg"
+
 	pb "github.com/maguec/GRPC-Valkey/proto/helloworld"
 )
 
-func worker() {
+var args struct {
+	Runs      int `help:"number of run throughs" default:"100" arg:"env:RUN_COUNT"`
+	Workers      int `help:"number of threads" default:"100" arg:"env:RUN_COUNT"`
+}
+
+func worker(tach *tachymeter.Tachymeter, mm *metermaid.Metermaid, runs int) {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials())) // Note: Use TLS in production
 	if err != nil {
@@ -23,13 +33,16 @@ func worker() {
 	c := pb.NewGreeterClient(conn)
 
 	name := "world"
-	for i := 0; i < 100; i++ {
+	for i := 0; i < runs; i++ {
 		// Contact the server and print out its response.
 		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 		defer cancel()
 
-		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
+		now := time.Now()
 		_, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		tach.AddTime(time.Since(now))
+		mm.Add()
 		if err != nil {
 			log.Fatalf("could not greet: %v", err)
 		}
@@ -39,13 +52,31 @@ func worker() {
 
 func main() {
 	wg := sync.WaitGroup{}
-	for i := 0; i < 100; i++ {
+  arg.MustParse(&args)
+	tach := tachymeter.New(&tachymeter.Config{Size: args.Runs*args.Workers})
+	mm := metermaid.New(&metermaid.Config{Size: args.Runs*args.Workers})
+	for i := 0; i < args.Workers; i++ {
 		wg.Add(1)
 		go func() {
-			worker()
+			worker(tach, mm, args.Runs)
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-
+	results := tach.Calc()
+	fmt.Println("------------------ Latency ------------------")
+	fmt.Printf(
+		"Max:\t\t%s\nMin:\t\t%s\nP95:\t\t%s\nP99:\t\t%s\nP99.9:\t\t%s\n\n",
+		results.Time.Max,
+		results.Time.Min,
+		results.Time.P95,
+		results.Time.P99,
+		results.Time.P999,
+	)
+	rates := mm.Calc()
+	fmt.Println("-------------------- Rate -------------------")
+	fmt.Printf(
+		"MaxRate:\t%.1f/s\nMinRate:\t%.1f/s\nP95Rate:\t%.1f/s\nP99Rate:\t%.1f/s\nP99.9Rate:\t%.1f/s\n",
+		rates.MaxRate, rates.MinRate, rates.P95Rate, rates.P99Rate, rates.P999Rate,
+	)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/maguec/GRPC-Valkey
 go 1.24.2
 
 require (
+	github.com/alexflint/go-arg v1.5.1
+	github.com/jamiealquiza/tachymeter v2.0.0+incompatible
+	github.com/maguec/metermaid v0.0.0-20221205163729-b17bc36f6238
 	github.com/valkey-io/valkey-go v1.0.61
 	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476
 	google.golang.org/grpc v1.73.0
@@ -10,6 +13,8 @@ require (
 )
 
 require (
+	github.com/alexflint/go-scalar v1.2.0 // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
