757a627220cd38b6c2110c9de6670d1256eeb14a
Chris Mague <github@mague.com>
2025-06-25 07:35:29 -0700

    shared client option

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
@@ -23,55 +24,83 @@ const (
 // Define a struct that implements the generated gRPC service interface
 type server struct {
 	pb.UnimplementedGreeterServer
-	valkeyClients []valkey.Client
+	valkeyClient valkey.Client
 }
 
-func dialLogger(s1 string, dialer *net.Dialer, tlsconfig *tls.Config) (net.Conn, error) {
+func retrieveTokenFunc(yo valkey.AuthCredentialsContext) (valkey.AuthCredentials, error) {
+	username := "default"
+	password := os.Getenv("TOKEN")
+	return valkey.AuthCredentials{Username: username, Password: password}, nil
+}
+
+func valkeyDialLogger(ctx context.Context, s1 string, dialer *net.Dialer, tlsconfig *tls.Config) (net.Conn, error) {
+	var conn net.Conn
+	var err error
 	now := time.Now()
-	conn, err := net.Dial("tcp", s1)
-	fmt.Println("Dialing", s1, "took", time.Since(now).Milliseconds(), "ms")
+	if tlsconfig != nil {
+		tlsDialer := tls.Dialer{NetDialer: dialer, Config: tlsconfig}
+		conn, err = tlsDialer.DialContext(ctx, "tcp", s1)
+	} else {
+		conn, err = net.Dial("tcp", s1)
+	}
+	log.Printf("Dialing %s took %v", s1, time.Since(now))
 	return conn, err
 }
 
 // Implement the SayHello method
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-  c := rand.Intn(10)
-  fmt.Println("Using client", c)
-  client := s.valkeyClients[c]
-	cmds := make(valkey.Commands, 0, 100)
+	var err error
+	keys := make([]string, 0, 100)
 	for i := 0; i < 10; i++ {
-		mykey := fmt.Sprintf("key-%d", rand.Intn(100000))
-		cmds = append(cmds, client.B().Set().Key(mykey).Value("val").Build())
+		keys = append(keys, fmt.Sprintf("key-%d", rand.Intn(100000)))
 	}
-	for _, resp := range client.DoMulti(ctx, cmds...) {
-		if err := resp.Error(); err != nil {
-			panic(err)
-		}
+
+  //err = s.valkeyClient.Do(ctx, s.valkeyClient.B().Mget().Key(keys...).Build()).Error()
+  err = s.valkeyClient.Do(ctx, s.valkeyClient.B().Get().Key(keys[0]).Build()).Error()
+	if err != nil {
+		return nil, err
+	} else {
+		return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+
 	}
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
-  clients := make([]valkey.Client, 10)
-	for i := 0; i < 10; i++ {
-		client, err := valkey.NewClient(
-			valkey.ClientOption{
-				InitAddress: []string{"127.0.0.1:30001"},
-				DialFn:      dialLogger,
-			},
-		)
-		if err != nil {
-			panic(err)
-		}
-		defer client.Close()
-    clients[i] = client
+	client, err := valkey.NewClient(
+		valkey.ClientOption{
+			InitAddress: []string{"127.0.0.1:30001"},
+			//DisableAutoPipelining:              false,
+			PipelineMultiplex: 8,
+			DialCtxFn:         valkeyDialLogger,
+			//AuthCredentialsFn: retrieveTokenFunc,
+			//EnableCrossSlotMGET:                true,
+			//AllowUnstableSlotsForCrossSlotMGET: false,
+			//SendToReplica: func(cmd valkey.Command) bool {
+			//	return cfg.readReplica && cmd.IsReadOnly() && rand.Float64() < 0.5
+			//},
+			//TLSConfig: &tls.Config{
+			//	RootCAs:            nil,
+			//	ClientSessionCache: nil,
+			//},
+		},
+	)
+	defer client.Close()
+	if err != nil {
+		panic(err)
 	}
+
+	log.Printf("Populating keyspace - starting")
+	for i := 0; i < 100000; i++ {
+		client.Do(context.Background(), client.B().Set().Key(fmt.Sprintf("key-%d", i)).Value(fmt.Sprintf("%d", i)).Build())
+	}
+	log.Printf("Populating keyspace - complete")
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{valkeyClients: clients})
+	pb.RegisterGreeterServer(s, &server{valkeyClient: client})
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
